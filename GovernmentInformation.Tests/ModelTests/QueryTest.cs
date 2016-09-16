34e48b8a22fe5230dce9e77805941423f413e1a7
Joel Delight <thefencingflutist@gmail.com>
2016-09-16 09:25:19 -0700

    Clean up views for queries and add a test for query model.

---
+++ GovernmentInformation.Tests/ModelTests/QueryTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using GovernmentInformation.Models;
+
+namespace GovernmentInformation.Tests
+{
+    public class QueryTest
+    {
+        [Fact]
+        public void GetDescriptionTest()
+        {
+            //Arrange
+            var query = new Query();
+            query.Description = "Is dog clean?";
+
+            //Act
+            var result = query.Description;
+
+            //Assert
+            Assert.Equal("Is dog clean?", result);
+        }
+    }
+}
