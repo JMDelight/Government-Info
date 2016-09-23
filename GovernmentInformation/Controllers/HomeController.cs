c54a30432612ff72a0ad2e2c67eb93f493c40fb3
Joel Delight <thefencingflutist@gmail.com>
2016-09-23 10:10:12 -0700

    create an Api call test function.

--- GovernmentInformation/Controllers/HomeController.cs
+++ GovernmentInformation/Controllers/HomeController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+using RestSharp.Authenticators;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 
 namespace GovernmentInformation.Controllers
 {
@@ -30,6 +34,22 @@ namespace GovernmentInformation.Controllers
         public IActionResult Error()
         {
             return View();
+        }
+
+        public void ApiTest()
+        {
+            var client = new RestClient("http://congress.api.sunlightfoundation.com/legislators/locate");
+            var request = new RestRequest("?zip=97229&apikey=d5977756ff384a7da59c8e860379b4bd");
+            client.ExecuteAsync(request, response =>
+            {
+                JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(response.Content);
+            });
+            //Task.Run(async () =>
+            //{
+            //    response = await GetResponseContentAsync(client, request) as RestResponse;
+            //}).Wait();
+            //JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(response.Content);
+            
         }
     }
 }
